d03e3f6c18860a7fd653ed0f33546969fc172d70
Thandepzai <cuthan2k1@gmail.com>
2024-09-30 00:27:16 +0700

    add some

---
+++ _next/static/chunks/app/layout-6f2a3b00574e3d2b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5433:function(n,e,t){Promise.resolve().then(t.t.bind(t,288,23)),Promise.resolve().then(t.t.bind(t,9051,23)),Promise.resolve().then(t.t.bind(t,5291,23))},5291:function(){},9051:function(){},288:function(){}},function(n){n.O(0,[431,971,23,744],function(){return n(n.s=5433)}),_N_E=n.O()}]);
